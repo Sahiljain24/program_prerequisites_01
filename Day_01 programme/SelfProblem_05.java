f04708b72a5f5b28db2f581dff56e01edc4eec6f
Sahiljain24 <24sahilj@gmail.com>
2025-01-08 11:17:25 +0530

    last problem for the day solved

---
+++ Day_01 programme/SelfProblem_05.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SelfProblem_05 {
+    public static void main(String[] args) {
+        // Convert Kilometers to Miles
+        Scanner sc = new Scanner(System.in);
+       double Kilometers = sc.nextInt();
+
+
+     Double  Miles = Kilometers * 0.621371;
+     System.out.println(Miles);
+    }
+}
