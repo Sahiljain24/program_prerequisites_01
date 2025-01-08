57439df28952271b093bc66287170581e9700a77
Sahiljain24 <24sahilj@gmail.com>
2025-01-08 11:06:53 +0530

    self_problem Solved

--- Day_01 programme/SelfProblem_03.java
+++ Day_01 programme/SelfProblem_03.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class SelfProblem_03 {
-    
+    public static void main(String[] args) {
+        // Power Calculation
+        Scanner sc = new Scanner(System.in);
+        int base = sc.nextInt();
+        int exponent = sc.nextInt();
+        System.out.println(Math.pow(base, exponent));
+    }
 }
